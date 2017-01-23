8b53885582527c3a66d9cb83fc2b287880d8a12b
vensilya@gmail.com <vensilya@gmail.com>
2017-01-23 22:29:55 +0300

    remove users route

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,21 +21,8 @@ from rest_framework import routers, serializers, viewsets
 from restapi import views as restapi_views
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^test/$', restapi_views.test_view),
+    url(r'^test/$', restapi_views.test),
     url(r'^api-token-auth/', views.obtain_auth_token),
     url(r'^admin/', admin.site.urls),
 ]
--- backend/restapi/views.py
+++ backend/restapi/views.py
@@ -5,6 +5,5 @@ from rest_framework.decorators import api_view
 # Create your views here.
 
 @api_view(['GET', 'POST'])
-def test_view(request):
-    print(request.user.is_authenticated())
+def test(request):
     return Response({'result': 'success'})
